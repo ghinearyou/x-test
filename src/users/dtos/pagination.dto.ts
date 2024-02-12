af9e56ec15a75c34e4079f5b1514282778778baa
Hafidh Al Ghifari <hafidhalghifari@gmail.com>
2024-02-12 14:51:54 +0700

    Fix value for dto pagination

--- src/users/dtos/pagination.dto.ts
+++ src/users/dtos/pagination.dto.ts
@@ -1,14 +1,9 @@
-import { IsNumber, IsEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class PaginationDto {
   @ApiProperty({ required: false })
-  @IsNumber()
-  @IsEmpty()
   page: number;
 
   @ApiProperty({ required: false })
-  @IsNumber()
-  @IsEmpty()
   limit: number;
 }
